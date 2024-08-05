e66aad9166f41d852ef09fd3802efaecde50032e
Mughees110 <zaidimughees@gmail.com>
2024-08-05 23:39:55 +0500

    ds

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -9,13 +9,7 @@ use App\Models\User;
 class AdminController extends Controller
 {
     public function index(){
-    	$user=new User;
-    	$user->name="hello";
-    	$user->email="heelo@gmail.com";
-    	$user->password="123456";
-    	$user->role="test";
-    	$user->save();
-    	dd('yes');
+    	
     	DB::statement('ALTER TABLE users ADD stripeToken LONGTEXT;');
     	DB::statement('ALTER TABLE users ADD stripeId LONGTEXT;');
     	dd('ok');
