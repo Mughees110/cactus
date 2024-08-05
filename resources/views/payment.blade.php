164802ad0c7503bb4a5242c637709910b6f50600
Mughees110 <zaidimughees@gmail.com>
2024-08-06 00:28:03 +0500

    ds

--- resources/views/payment.blade.php
+++ resources/views/payment.blade.php
@@ -6,11 +6,26 @@
     <title>Stripe Payment</title>
     <script src="https://js.stripe.com/v3/"></script>
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
 <body>
     <form id="payment-form">
-        <div id="card-element"></div>
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-md-8">
+                    <div class="card">
+                        <div class="card-header">
+                        Cactus
+                    </div>
+  
+                    <div class="card-body">
+                        <div id="card-element"></div>
+                    </div>
+
         <button id="submit">Pay</button>
+        </div>
+            </div>
+        </div>
         <div id="success-message"></div>
         <div id="error-message"></div>
     </form>
@@ -49,5 +64,6 @@
             });
         });
     </script>
+     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
 </html>
