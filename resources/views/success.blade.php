bbab1ef5fb8924c30e41bdfc7321dc351ea0de17
Mughees110 <zaidimughees@gmail.com>
2024-08-06 19:04:12 +0500

    ds

--- resources/views/success.blade.php
+++ resources/views/success.blade.php
@@ -1,4 +1,4 @@
 <p style="display: flex;
     justify-content: center; /* Centers horizontally */
     align-items: center;     /* Centers vertically */
-    height: 100vh;">¡La información de la tarjeta se almacenó exitosamente!</p>
+    height: 100vh;font-size: 50px;text-align: center;">¡La información de la tarjeta se almacenó exitosamente!</p>
