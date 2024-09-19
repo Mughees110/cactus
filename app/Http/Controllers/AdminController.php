e56cc0844222cc7f2a1a074b7bd04ebef2f1592f
Mughees110 <zaidimughees@gmail.com>
2024-09-19 19:20:54 +0500

    ds

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -6,11 +6,12 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Carbon\Carbon;
 use App\Models\User;
+use App\Models\Rating;
 class AdminController extends Controller
 {
     public function index(){
-    	
-    	DB::statement('ALTER TABLE users ADD fcm LONGTEXT;');
-    	dd('ok');
+    	$ratings=Rating::all();
+    	//DB::statement('ALTER TABLE users ADD fcm LONGTEXT;');
+    	dd($ratings);
     }
 }
